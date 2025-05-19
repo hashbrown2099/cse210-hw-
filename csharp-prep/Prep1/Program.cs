24262d41c0c55151241702a9fbe6933f51d0aaf5
Aaron Hahsbrown <aaron.social2099@gmail.com>
2025-05-18 20:04:03 -0600

    Prep 1 Finished

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name?");    // Ask the user for their first name 
+        string first_name = Console.ReadLine();      // Defines the first_name as a string and has the user define it
+        Console.Write("what is your last name?");   //  Ask the user for their first name   
+        string Last_name = Console.ReadLine();      // Degines Last_name as a string and allows user repsonse to define the cvariable
+        Console.WriteLine($"Your name is {Last_name}, {first_name} {Last_name}."); // Prints users first then full name in james bond format
+
+    }   
     }
-}
